5064b22b9728133a9756fa97294cb7e5d5cccaf0
roberto <roberto.schifano01@universitadipavia.it>
2025-08-21 21:43:00 +0200

    MapReduce notebook

--- MapReduce Job/mapper.py
+++ MapReduce Job/mapper.py
@@ -8,54 +8,11 @@ for line in sys.stdin:
   splits=line.split("', '")
 
   #first dataset
-  book_id = "-"
   title = "-"
-  authors = "-"
-  isbn = "-"
-  isbn13 = "-"
-  language_code = "-"
-  num_pages = "-"
-  ratings_count = "-"
-  ratings_average = "-"
-  text_reviews_count = "-"
-  publication_date = "-"
-  publisher = "-"
-
-  #second dataset
-  book_format = "-"
-  description = "-"
-  genre = "-"
-  image = "-"
-  link = "-"
 
   if len(splits)==12:
-    book_id = splits[0]
     title = splits[1]
-    authors = splits[2]
-    ratings_average = splits[3]
-    isbn = splits[4]
-    isbn13 = splits[5]
-    language_code = splits[6]
-    num_pages = splits[7]
-    ratings_count = splits[8]
-    text_reviews_count = splits[9]
-    publication_date = splits[10]
-    publisher = splits[11]
-  elif len(splits)==13:
-    authors= splits[0]
-    book_format = splits[1]
-    description = splits[2]
-    genre = splits[3]
-    image = splits[4]
-    isbn = splits[5]
-    isbn13 = splits[6]
-    link = splits[7]
-    num_pages = splits[8]
-    ratings_average = splits[9]
-    text_reviews_count = splits[10]
-    title = splits[11]
-    ratings_count = splits[12]
   else:
     continue
 
-  print(isbn, "\t", book_id, "\t", language_code, "\t", publication_date, "\t", publisher, "\t", isbn13, "\t", title, "\t", authors, "\t", description, "\t", book_format, "\t", num_pages, "\t", genre, "\t", image, "\t", link, "\t", ratings_count, "\t", ratings_average, "\t", text_reviews_count)
+  print(title.lower(), "\t", 1)
--- MapReduce Job/reducer.py
+++ MapReduce Job/reducer.py
@@ -3,52 +3,25 @@
 import sys
 import string
 
-last_isbn = None
-
 # first dataset
-cur_book_id = "-"
-cur_title = "-"
-cur_authors = "-"
-cur_isbn = "-"
-cur_isbn13 = "-"
-cur_language_code = "-"
-cur_num_pages = "-"
-cur_ratings_count = "-"
-cur_ratings_average = "-"
-cur_text_reviews_count = "-"
-cur_publication_date = "-"
-cur_publisher = "-"
-
-# second dataset
-cur_book_format = "-"
-cur_description = "-"
-cur_genre = "-"
-cur_image = "-"
-cur_link = "-"
+cur_title = None
+title = None
+cur_count = 0
 
 for line in sys.stdin:
   line = line.strip()
 
-  isbn, book_id, language_code, publication_date, publisher, isbn13, title, authors, description, book_format, num_pages, genre, image, link, ratings_count, ratings_average, text_reviews_count= line.split("\t")
+  title, count = line.split("\t", 1)
+  count=int(count)
 
-  if not last_isbn or isbn != last_isbn:
-    last_isbn = isbn
-    cur_book_id = book_id
+  if cur_title == title:
+    cur_count += count
+  else:
+    if cur_title:
+      print(cur_title, "\t", cur_count)
+
     cur_title = title
-    cur_authors = authors
-    cur_isbn13 = isbn13
-    cur_language_code = language_code
-    cur_num_pages = num_pages
-    cur_ratings_count = ratings_count
-    cur_ratings_average = ratings_average
-    cur_text_reviews_count = text_reviews_count
-    cur_publication_date = publication_date
-    cur_publisher = publisher
-    cur_book_format = book_format
-    cur_description = description
-    cur_genre = genre
-    cur_image = image
-    cur_link = link
+    cur_count = count
 
-  elif isbn == last_isbn:
-    print(isbn, cur_book_id, cur_language_code, cur_publication_date, cur_publisher, cur_isbn13, title, authors, description, book_format, num_pages, genre, image, link, cur_ratings_count, cur_ratings_average, cur_text_reviews_count)
+if cur_title:
+  print(cur_title, "\t", cur_count)
